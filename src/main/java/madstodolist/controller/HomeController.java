581f8d5b3c0edc16a34d64ac81a78e13dd473910
alejandro06020 <alejandro.minda@epn.edu.ec>
2025-05-31 16:08:22 -0500

    Barra de menu
    Se agrego la barra de menu a todos las paginas menos al login y al registro. Se agrego una nueva pagina aboutIn. Se agregaron 2 nuevos tests uno para la nueva pagina aboutIn y uno Para testear el nuevo redireccionamiento get de para editar tareas

--- src/main/java/madstodolist/controller/HomeController.java
+++ src/main/java/madstodolist/controller/HomeController.java
@@ -1,13 +1,41 @@
 package madstodolist.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.controller.exception.UsuarioNoLogeadoException;
+import madstodolist.dto.UsuarioData;
+import madstodolist.service.UsuarioService;
 
 @Controller
 public class HomeController {
+	@Autowired
+	UsuarioService usuarioService;
+    @Autowired
+    ManagerUserSession managerUserSession;
+
+    private void comprobarUsuarioLogeado(Long idUsuario) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (!idUsuario.equals(idUsuarioLogeado))
+            throw new UsuarioNoLogeadoException();
+    }
+    //Esta pagina about se mostrara a los usuarios no logeados
     @GetMapping("/about")
     public String about(Model model) {
         return "about";
+    }
+    //Creamos una nueva pagina about para usuarios logeados
+    @GetMapping("/usuarios/{id}/aboutIn")
+    public String aboutIn(@PathVariable(value="id") Long idUsuario,Model model,HttpSession session) {
+    	comprobarUsuarioLogeado(idUsuario);
+    	UsuarioData usuario = usuarioService.findById(idUsuario);
+    	model.addAttribute("usuario", usuario);
+    	return "aboutIn";
     }
 }
